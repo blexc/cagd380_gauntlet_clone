61c61467ca5e33e5f908082b75a0ae2aee461216
alex <alex.tchap99@gmail.com>
2021-04-29 13:58:21 -0700

    a moving wizard

---
+++ Assets/Scripts/Players/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] protected int score;
+    [SerializeField] protected int health;
+    [SerializeField] protected int damage;
+    [SerializeField] protected int armor;
+    [SerializeField] protected int movementSpeed;
+    [SerializeField] protected int attackSpeed;
+    protected List<Item> inventory = new List<Item>();
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical"); 
+        Vector3 dir = new Vector3(horizontal, 0f, vertical);
+
+        rb.velocity = movementSpeed * Time.fixedDeltaTime * dir;
+    }
+
+    public void AddItem(GameObject item)
+    {
+        // TODO 
+    }
+
+    public void TakeDamage(int value)
+    {
+        // TODO 
+    }
+
+    protected virtual void MeleeAttack()
+    {
+        // TODO 
+    }
+
+    protected virtual void ProjectileAttack()
+    {
+        // TODO 
+    }
+}
