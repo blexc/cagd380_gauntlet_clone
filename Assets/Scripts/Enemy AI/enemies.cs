9eea3e78699a0c37382497cccf80028b05198fcc
RedThunder5227 <ezioisabadass@gmail.com>
2021-05-03 16:49:50 -0700

    class made, cameras set

---
+++ Assets/Scripts/Enemy AI/enemies.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemies : MonoBehaviour
+{
+    //variables
+    int health;
+    int damage;
+    int moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //functions
+    void Movement()
+    { 
+    
+    }
+
+    void Attack()
+    { 
+    
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+}
