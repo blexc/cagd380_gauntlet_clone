296e7f14cd9f50208ba9d28eb446253025f8cd6b
alex <alex.tchap99@gmail.com>
2021-05-10 17:37:19 -0700

    items, keys, doors, potions, food, and artifacts
     - key and door mechanics

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        var player = other.gameObject.GetComponent<Player>();
+        if (player)
+        {
+            player.AddItem(this);
+
+            // don't remove the gameobject, but make it not interactible
+            GetComponent<SphereCollider>().enabled = false;
+            GetComponent<MeshRenderer>().enabled = false;
+        }
+    } 
+}
