d84e5b3c990f81674538361bde5fd72a5bff8264
RedThunder5227 <ezioisabadass@gmail.com>
2021-05-13 17:17:49 -0700

    camera and multiplayer

--- Assets/Scripts/Players/Warrior.cs
+++ Assets/Scripts/Players/Warrior.cs
@@ -4,4 +4,21 @@ using UnityEngine;
 
 public class Warrior : Player
 {
+    public static Warrior instance;
+
+    protected override void Start()
+    {
+        // ensure only one instance
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        base.Start();
+    }
 }
--- Assets/Scripts/Players/Wizard.cs
+++ Assets/Scripts/Players/Wizard.cs
@@ -4,4 +4,21 @@ using UnityEngine;
 
 public class Wizard : Player
 {
+    public static Wizard instance;
+
+    protected override void Start()
+    {
+        // ensure only one instance
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        base.Start();
+    }
 }
